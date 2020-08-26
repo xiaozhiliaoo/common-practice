b57eb985cd7ed1c089cb8fe9529535ef1177c44a
lili <xiaozhiliaoo@gmail.com>
2020-08-27 01:57:51 +0800

    add CharMatcherTest

---
+++ src/main/java/org/lili/google/guava/CharMatcherTest.java
@@ -0,0 +1,57 @@
+package org.lili.google.guava;
+
+import com.google.common.base.CharMatcher;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author lili
+ * @date 2020/8/27 0:12
+ * @description
+ * @notes
+ */
+
+public class CharMatcherTest {
+    @Test
+    public void whenRemoveSpecialCharacters_thenRemoved(){
+        String input = "H*el.lo,}12";
+        CharMatcher matcher = CharMatcher.javaLetterOrDigit();
+        String result = matcher.retainFrom(input);
+        assertEquals("Hello12", result);
+    }
+
+    @Test
+    public void whenRemoveNonASCIIChars_thenRemoved() {
+        String input = "あhello₤";
+
+        String result = CharMatcher.ascii().retainFrom(input);
+        assertEquals("hello", result);
+
+        result = CharMatcher.inRange('0', 'z').retainFrom(input);
+        assertEquals("hello", result);
+    }
+
+    @Test
+    public void whenReplaceFromString_thenReplaced() {
+        String input = "apple-banana.";
+
+        String result = CharMatcher.anyOf("-.").replaceFrom(input, '!');
+        assertEquals("apple!banana!", result);
+
+        result = CharMatcher.is('-').replaceFrom(input, " and ");
+        assertEquals("apple and banana.", result);
+
+        String remove = StringUtils.remove("www.baidu.com////`/coCommon/forward", "/coCommon/forward");
+        System.out.println("remove"+ remove);
+
+
+        System.out.println("www.baidu.com////`/coCommon/forward".replaceAll("/coCommon/forward", ""));
+
+        CharMatcher any = CharMatcher.noneOf("/coCommon/forward");
+        String s = any.retainFrom("www.baidu.com////`/coCommon/forward");
+        System.out.println(s);
+
+    }
+}
--- src/main/java/org/lili/jdk/ArrayTest.java
+++ src/main/java/org/lili/jdk/ArrayTest.java
@@ -1,6 +1,12 @@
 package org.lili.jdk;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author lili
@@ -14,6 +20,46 @@ public class ArrayTest {
     public void test() {
         String[] ips = {"112.17.245.74", "128.1.48.108", "10.1.50.27", "10.1.24.167"};
         String ip = ips[ips.length - 2];
-        System.out.println(ip);
+        System.out.println(ip);//10.1.50.27
+
+        String[] ips2 = {"112.17.245.74", "128.1.48.108", "10.1.50.27", "10.1.24.167","56.43.23.55"};
+        String ip2 = ips2[ips2.length - 2];
+        System.out.println(ip2); //10.1.24.167
+
+
+        String ipstr = "112.17.245.74,128.1.48.108,10.1.50.27,10.1.24.167,56.43.23.55,192.168.11.120";
+        System.out.println(getRewriteIP(ipstr));
+
+    }
+
+    public static boolean isIntranet(String ip) {
+        String reg = "^(192\\.168|172\\.(1[6-9]|2\\d|3[0,1]))(\\.(2[0-4]\\d|25[0-5]|[0,1]?\\d?\\d)){2}$|^10(\\.([2][0-4]\\d|25[0-5]|[0,1]?\\d?\\d)){3}$|^127(\\.\\d+){3}$";
+        Pattern p = Pattern.compile(reg);
+        Matcher matcher = p.matcher(ip);
+        return matcher.find();
+    }
+
+
+    public static String getRewriteIP(String ipStr) {
+        String[] split = ipStr.split(",");
+        List<String> newIPs = new ArrayList<>();
+        if (split.length > 2) {
+            for (int i = split.length - 1; i >= 0; i--) {
+                if (!isIntranet(StringUtils.trim(split[i]))) {
+                    newIPs.add(StringUtils.trim(split[i]));
+                }
+            }
+            List<String> newIPs2 = new ArrayList<>();
+            for (int j = newIPs.size()-1; j >= 0; j--) {
+                newIPs2.add(newIPs.get(j));
+            }
+            if (newIPs2.size() <= 0) {
+                newIPs2.add(StringUtils.trim(split[0]));
+            }
+
+            ipStr = StringUtils.join(newIPs2, ",");
+        }
+
+        return ipStr;
     }
 }
