3fe84f986d21d9439b383b8a071cfd26352f3c49
lili <xiaozhiliaoo@gmail.com>
2020-11-14 23:44:24 +0800

    update README

---
+++ src/main/java/org/lili/jdk/jcf/AbstractData.java
@@ -0,0 +1,19 @@
+package org.lili.jdk.jcf;
+
+/**
+ * @author lili
+ * @date 2020/11/14 18:20
+ * @see
+ * @since
+ */
+public abstract class AbstractData {
+
+    private int a;
+
+    abstract int get();
+
+    public AbstractData() {
+
+    }
+
+}
